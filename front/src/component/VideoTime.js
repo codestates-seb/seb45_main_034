adf08a050397a2ae2e4716d28dacec9028e40549
Explorers <129934405+Explorers12@users.noreply.github.com>
2023-09-22 15:56:43 +0900

    Delete front directory

--- front/src/component/VideoTime.js
+++
@@ -1,6 +0,0 @@
-export default function VideoTime(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time % 60);
-    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  }
-  
--- front/src/type/modalType.ts
+++
@@ -1,6 +0,0 @@
-export type PopupModaltype = {
-    title: string;
-    message: string;
-    onDeleteClick: () => void;
-    onCancelClick: () => void;
-}
