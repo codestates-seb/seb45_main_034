f357a0b323cb1975f829925c93b95b65a79711dc
Explorers <129934405+Explorers12@users.noreply.github.com>
2023-09-22 15:56:12 +0900

    Delete front directory

--- front/src/component/videoduration.ts
+++
@@ -1,9 +0,0 @@
-export function formatVideoDuration(durationInSeconds: number): string {
-    const minutes = Math.floor(durationInSeconds / 60);
-    const seconds = Math.floor(durationInSeconds % 60);
-  
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(seconds).padStart(2, '0');
-  
-    return `${formattedMinutes}:${formattedSeconds}`;
-  }
